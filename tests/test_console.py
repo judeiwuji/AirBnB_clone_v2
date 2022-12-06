759d3af69efdd03071ce8b1afaa26c316ae06c8a
jude iwuji <judeiwuji@gmail.com>
2022-12-06 07:03:13 +0100

    create related objects

--- tests/test_console.py
+++ tests/test_console.py
@@ -28,7 +28,8 @@ class TestHBNBCommandDoCreate(unittest.TestCase):
 
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd('create User first_name="john"\
-                                  last_name="doe"')
+                                  last_name="doe" email="john@doe"\
+                                  password="doesecret"')
             obj_id = f.getvalue().strip()
             obj = storage.all().get('User.{}'.format(obj_id), None)
             self.assertIsInstance(obj, User)
@@ -49,25 +50,42 @@ class TestHBNBCommandDoCreate(unittest.TestCase):
         """Test create city"""
 
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd('create City name="victoria_island"')
-            obj_id = f.getvalue().strip()
+            HBNBCommand().onecmd('create State name="Abuja"')
+            state_id = f.getvalue().strip()
+            cmd = 'create City name="gwagwalanda" state_id={}'.\
+                  format(state_id)
+            HBNBCommand().onecmd(cmd)
+            obj_id = f.getvalue().strip().splitlines()[-1]
             obj = storage.all().get('City.{}'.format(obj_id), None)
             self.assertIsInstance(obj, City)
-            self.assertEqual(obj.name, "victoria island")
+            self.assertEqual(obj.name, "gwagwalanda")
 
     def test_create_place(self):
         """Test create place"""
 
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd('create Place city_id="0001" user_id="0001"\
+            HBNBCommand().onecmd('create State name="Umuahaia"')
+            state_id = f.getvalue().strip()
+            cmd = 'create City name="aba" state_id={}'.\
+                  format(state_id)
+            HBNBCommand().onecmd(cmd)
+            city_id = f.getvalue().strip().splitlines()[-1]
+            cmd = 'create User first_name="jane"\
+                  last_name="doe" email="jane@doe"\
+                  password="janesecret"'
+            HBNBCommand().onecmd(cmd)
+            user_id = f.getvalue().strip().splitlines()[-1]
+
+            HBNBCommand().onecmd('create Place city_id="{}" user_id="{}"\
                 name="My_little_house" number_rooms=4 number_bathrooms=2\
                 max_guest=10 price_by_night=300 latitude=37.773972\
-                longitude=-122.431297')
-            obj_id = f.getvalue().strip()
+                longitude=-122.431297'.format(city_id, user_id))
+            obj_id = f.getvalue().strip().splitlines()[-1]
+
             obj = storage.all().get('Place.{}'.format(obj_id), None)
             self.assertIsInstance(obj, Place)
-            self.assertEqual(obj.city_id, "0001")
-            self.assertEqual(obj.user_id, "0001")
+            self.assertEqual(obj.city_id, city_id)
+            self.assertEqual(obj.user_id, user_id)
             self.assertEqual(obj.name, "My little house")
             self.assertEqual(obj.number_rooms, 4)
             self.assertEqual(obj.number_bathrooms, 2)
@@ -80,13 +98,32 @@ class TestHBNBCommandDoCreate(unittest.TestCase):
         """Test create review"""
 
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd('create Review place_id="0001" user_id="0001"\
-                                  text="nice_place"')
-            obj_id = f.getvalue().strip()
+            HBNBCommand().onecmd('create State name="Edo"')
+            state_id = f.getvalue().strip()
+            cmd = 'create City name="Benin" state_id={}'.\
+                  format(state_id)
+            HBNBCommand().onecmd(cmd)
+            city_id = f.getvalue().strip().splitlines()[-1]
+            cmd = 'create User first_name="john"\
+                  last_name="smith" email="john@smith"\
+                  password="johnsecret"'
+            HBNBCommand().onecmd(cmd)
+            user_id = f.getvalue().strip().splitlines()[-1]
+
+            HBNBCommand().onecmd('create Place city_id="{}" user_id="{}"\
+                name="My_little_house" number_rooms=4 number_bathrooms=2\
+                max_guest=10 price_by_night=300 latitude=37.773972\
+                longitude=-122.431297'.format(city_id, user_id))
+            place_id = f.getvalue().strip().splitlines()[-1]
+
+            HBNBCommand().onecmd('create Review place_id="{}" user_id="{}"\
+                                  text="nice_place"'.format(place_id, user_id))
+            obj_id = f.getvalue().strip().splitlines()[-1]
+
             obj = storage.all().get('Review.{}'.format(obj_id), None)
             self.assertIsInstance(obj, Review)
-            self.assertEqual(obj.place_id, "0001")
-            self.assertEqual(obj.user_id, "0001")
+            self.assertEqual(obj.place_id, place_id)
+            self.assertEqual(obj.user_id, user_id)
             self.assertEqual(obj.text, "nice place")
 
 
